0475f6e21d58ef33ba8a1fa9d03d2a37dd41a2f5
XiNGRZ <chenxingyu92@gmail.com>
2018-08-18 15:36:29 +0800

    oscar: Import stock cpufreq scaling governor settings
    Change-Id: Id1122d929ed83356ac6dd4a0401687c57fb86161

--- init/system/etc/init.qcom.post_boot.sh
+++ init/system/etc/init.qcom.post_boot.sh
@@ -1286,13 +1286,13 @@ case "$target" in
                 #governor settings
                 echo 1 > /sys/devices/system/cpu/cpu0/online
                 echo "interactive" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-                echo "19000 1401600:39000" > /sys/devices/system/cpu/cpufreq/interactive/above_hispeed_delay
+                echo "19000" > /sys/devices/system/cpu/cpufreq/interactive/above_hispeed_delay
                 echo 85 > /sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load
                 echo 20000 > /sys/devices/system/cpu/cpufreq/interactive/timer_rate
                 echo 1401600 > /sys/devices/system/cpu/cpufreq/interactive/hispeed_freq
                 echo 0 > /sys/devices/system/cpu/cpufreq/interactive/io_is_busy
-                echo "85 1401600:80" > /sys/devices/system/cpu/cpufreq/interactive/target_loads
-                echo 39000 > /sys/devices/system/cpu/cpufreq/interactive/min_sample_time
+                echo "60 1401600:80" > /sys/devices/system/cpu/cpufreq/interactive/target_loads
+                echo 20000 > /sys/devices/system/cpu/cpufreq/interactive/min_sample_time
                 echo 40000 > /sys/devices/system/cpu/cpufreq/interactive/sampling_down_factor
                 echo 652800 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
 
