f49c20e3e83648e8c93e14276ad719e27c538812
XiNGRZ <chenxingyu92@gmail.com>
2018-11-16 11:13:05 +0800

    oscar: Rename overlay-mk -> overlay-mokee
    Change-Id: I6f5f617a93faec9e093b00289abf66c7baa634ef

--- device.mk
+++ device.mk
@@ -26,7 +26,7 @@ $(call inherit-product, vendor/smartisan/oscar/oscar-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-mk
+    $(LOCAL_PATH)/overlay-mokee
 
 # Permissions
 PRODUCT_COPY_FILES += \
