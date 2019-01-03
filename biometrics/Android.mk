ce7a60a3129effc134467ff78d6f8304872c8e85
XiNGRZ <chenxingyu92@gmail.com>
2019-01-04 04:41:11 +0800

    oscar: Introduce custom fingerprint@2.0 service
    From hardware/mokee/interfaces/biometrics/fingerprint/2.0
    
    Change-Id: I0fdfb8dbf4ca5d9eae7322780270c6d5eb16649e

---
+++ biometrics/Android.mk
@@ -0,0 +1,21 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := android.hardware.biometrics.fingerprint@2.0-service.oscar
+LOCAL_INIT_RC := android.hardware.biometrics.fingerprint@2.0-service.oscar.rc
+LOCAL_VENDOR_MODULE := true
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_SRC_FILES := \
+    BiometricsFingerprint.cpp \
+    service.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+    libutils \
+    liblog \
+    libhidlbase \
+    libhidltransport \
+    libhardware \
+    libhwbinder \
+    android.hardware.biometrics.fingerprint@2.1
+
+include $(BUILD_EXECUTABLE)
