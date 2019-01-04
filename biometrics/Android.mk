6dd29b3bb86c47cab1ca79f7cc3ec87d0152895c
XiNGRZ <chenxingyu92@gmail.com>
2019-01-04 16:16:07 +0800

    oscar: biometrics: Import fingerprintd from N
    Change-Id: I7e9778bb9ddea078a9611bf19aeb368052cd5b36

--- biometrics/Android.mk
+++ biometrics/Android.mk
@@ -6,6 +6,9 @@ LOCAL_INIT_RC := android.hardware.biometrics.fingerprint@2.0-service.oscar.rc
 LOCAL_VENDOR_MODULE := true
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_SRC_FILES := \
+    fingerprintd/FingerprintDaemonProxy.cpp \
+    fingerprintd/IFingerprintDaemonCallback.cpp \
+    fingerprintd/IFingerprintDaemon.cpp \
     BiometricsFingerprint.cpp \
     service.cpp
 
